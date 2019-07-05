1858e9ade3dda123da56e00af896db3b3ec55fa2
Abhijit.Kulkarni <Abhijit.Kulkarni@iress.com.au>
2019-07-06 01:00:25 +1000

    deploy script

--- scripts/infra_deploy.sh
+++ scripts/infra_deploy.sh
@@ -3,10 +3,13 @@
 ENV="${ENV}"
 DESIRED_CAPACITY="${DESIRED_CAPACITY}"
 MAX_CAPACITY="${MAX_CAPACITY}"
+VPC_NAME=${VPC_NAME}
 
-VPC_ID
-PUBLIC_SUBNET_IDS
-PRIVATE_SUBNET_IDS
+VPC_ID=$(aws ec2 describe-vpcs --filters "Name='tag:Name',Values=${VPC_NAME}" --query 'Vpcs[0].VpcId')
+
+
+PUBLIC_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-003e06e33a87c22f5" "Name=tag:Tier, Values=public" --query 'Subnets[*].SubnetId')
+PRIVATE_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-003e06e33a87c22f5" "Name=tag:Tier, Values=private" --query 'Subnets[*].SubnetId')
 
 
 STACK_NAME="${ENV}-ecs-ec2"
