fc60b3e5e2fa7f5b5a221a12bbaccbd2b077b2bc
Abhijit.Kulkarni <Abhijit.Kulkarni@iress.com.au>
2019-07-06 14:08:03 +1000

    1. Make top 3 steps container,tag,publish 2. added names to each of the resources 3. output the ALB DNS

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ DOCKER_NETWORK = docker network
 NETWORK_NAME = kong-net
 NETWORK_SUBNET = 10.0.30.0/24
 
+export ENV=$(shell scripts/resolve_env.sh ENV)
 export VERSION_TAG ?= $(shell git rev-parse --short HEAD)
 export KONG_IMAGE_NAME = kong-api
 export APP_IMAGE_NAME = test-app
@@ -14,9 +15,26 @@ export AWS_ACCESS_KEY_ID=$(shell scripts/resolve_env.sh AWS_ACCESS_KEY_ID)
 export AWS_SECRET_ACCESS_KEY=$(shell scripts/resolve_env.sh AWS_SECRET_ACCESS_KEY)
 export AWS_SESSION_TOKEN=$(shell scripts/resolve_env.sh AWS_SESSION_TOKEN)
 
+export STACK_NAME=${ENV}-ecs-ec2
+
 export KONG_REPOSITORY_URL=${APP_ECR_REPOSITORY_URL}:${KONG_IMAGE_NAME}-${VERSION_TAG}
 export APP_REPOSITORY_URL=${APP_ECR_REPOSITORY_URL}:${APP_IMAGE_NAME}-${VERSION_TAG}
 
+
+container: kong_container app_container
+.PHONY: container
+
+tag: kong_container_tag app_container_tag
+.PHONY: tag
+
+publish: kong_publish app_publish
+.PHONY: publish
+
+infra: .network
+	docker-compose run infra
+
+infra_shell: .network
+	docker-compose run --entrypoint sh infra
 
 kong_container: .network
 	docker-compose build kongapi
@@ -49,12 +67,6 @@ app_publish: .network
 app_run: .network
 	docker-compose run -d -p 8080:8080 app
 .PHONY: app_run
-
-infra: .network
-	docker-compose run infra
-
-infra_shell: .network
-	docker-compose run --entrypoint sh infra
 
 .network:
 	$(DOCKER_NETWORK) inspect  $(NETWORK_NAME) > /dev/null 2>&1; \
--- scripts/infra_deploy.sh
+++ scripts/infra_deploy.sh
@@ -1,21 +1,15 @@
 #!/bin/sh -eu
 
-
 VPC_ID=$(aws ec2 describe-vpcs --filters "Name='tag:Name',Values=${VPC_NAME}" --query 'Vpcs[0].VpcId' | tr -d '"')
-
 
 PUBLIC_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-003e06e33a87c22f5" "Name=tag:Tier, Values=public" --query 'Subnets[*].SubnetId' | tr -d '"[]')
 PRIVATE_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-003e06e33a87c22f5" "Name=tag:Tier, Values=private" --query 'Subnets[*].SubnetId' | tr -d '"[]')
 
-echo $VPC_ID
-echo $PUBLIC_SUBNET_IDS
-echo $PRIVATE_SUBNET_IDS
-echo $KONG_REPOSITORY_URL
-echo $APP_REPOSITORY_URL
-
-STACK_NAME="${ENV}-ecs-ec2"
-
-aws cloudformation deploy --template-file ecs-ec2-kong-app.yml --stack-name ${STACK_NAME} --capabilities CAPABILITY_IAM --parameter-overrides \
-  "DesiredCapacity=${DESIRED_CAPACITY}" "MaxSize=${MAX_CAPACITY}" \
+aws cloudformation deploy --template-file ecs-ec2-kong-app.yml --stack-name ${STACK_NAME} --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides \
+  "Env=${ENV}" "DesiredCapacity=${DESIRED_CAPACITY}" "MaxSize=${MAX_CAPACITY}" \
   "KongImage=${KONG_REPOSITORY_URL}" "AppImage=${APP_REPOSITORY_URL}" \
-  "PublicSubnetId=${PUBLIC_SUBNET_IDS}" "PrivateSubnetId=${PRIVATE_SUBNET_IDS}" "VpcId=${VPC_ID}" "KeyName=abhi-ecs-ec2-access"
+  "PublicSubnetId=${PUBLIC_SUBNET_IDS}" "PrivateSubnetId=${PRIVATE_SUBNET_IDS}" "VpcId=${VPC_ID}"
+
+aws cloudformation describe-stacks --stack-name ${STACK_NAME}
+
