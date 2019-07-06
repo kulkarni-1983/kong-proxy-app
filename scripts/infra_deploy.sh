105b74b2d5aefae1fcce990fb6cdcb2d72fbeb60
Abhijit.Kulkarni <Abhijit.Kulkarni@iress.com.au>
2019-07-06 12:15:22 +1000

    cloudformation fails if 1. yml file has spaces before or after 'name' !!!!! 2. parameters are wrapped with quotes.

--- scripts/infra_deploy.sh
+++ scripts/infra_deploy.sh
@@ -1,19 +1,21 @@
 #!/bin/sh -eu
 
-ENV="${ENV}"
-DESIRED_CAPACITY="${DESIRED_CAPACITY}"
-MAX_CAPACITY="${MAX_CAPACITY}"
-VPC_NAME=${VPC_NAME}
 
-VPC_ID=$(aws ec2 describe-vpcs --filters "Name='tag:Name',Values=${VPC_NAME}" --query 'Vpcs[0].VpcId')
+VPC_ID=$(aws ec2 describe-vpcs --filters "Name='tag:Name',Values=${VPC_NAME}" --query 'Vpcs[0].VpcId' | tr -d '"')
 
 
-PUBLIC_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-003e06e33a87c22f5" "Name=tag:Tier, Values=public" --query 'Subnets[*].SubnetId')
-PRIVATE_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-003e06e33a87c22f5" "Name=tag:Tier, Values=private" --query 'Subnets[*].SubnetId')
+PUBLIC_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-003e06e33a87c22f5" "Name=tag:Tier, Values=public" --query 'Subnets[*].SubnetId' | tr -d '"[]')
+PRIVATE_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-003e06e33a87c22f5" "Name=tag:Tier, Values=private" --query 'Subnets[*].SubnetId' | tr -d '"[]')
 
+echo $VPC_ID
+echo $PUBLIC_SUBNET_IDS
+echo $PRIVATE_SUBNET_IDS
+echo $KONG_REPOSITORY_URL
+echo $APP_REPOSITORY_URL
 
 STACK_NAME="${ENV}-ecs-ec2"
-aws cloudformation deploy --template-file ecs-ec2-kong-app.yml -stack-name ${STACK_NAME} --parameter-overrides \
-  DesiredCapacity=${DESIRED_CAPACITY} MaxSize=${MAX_CAPACITY} \
-  KongImage=${KONG_REPOSITORY_URL} AppImage=${APP_REPOSITORY_URL} \
-  PublicSubnetId=${PUBLIC_SUBNET_IDS} PrivateSubnetId=${PRIVATE_SUBNET_IDS} VpcId=${VPC_ID} KeyName=""
+
+aws cloudformation deploy --template-file ecs-ec2-kong-app.yml --stack-name ${STACK_NAME} --capabilities CAPABILITY_IAM --parameter-overrides \
+  "DesiredCapacity=${DESIRED_CAPACITY}" "MaxSize=${MAX_CAPACITY}" \
+  "KongImage=${KONG_REPOSITORY_URL}" "AppImage=${APP_REPOSITORY_URL}" \
+  "PublicSubnetId=${PUBLIC_SUBNET_IDS}" "PrivateSubnetId=${PRIVATE_SUBNET_IDS}" "VpcId=${VPC_ID}" "KeyName=abhi-ecs-ec2-access"
