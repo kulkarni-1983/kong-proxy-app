8a6558b6f411176a517aa7d4e8c2bc48fcd1d68f
Abhijit.Kulkarni <Abhijit.Kulkarni@iress.com.au>
2019-07-12 20:41:21 +1000

    removed hardcoding of vpc

--- scripts/infra_deploy.sh
+++ scripts/infra_deploy.sh
@@ -2,8 +2,8 @@
 
 VPC_ID=$(aws ec2 describe-vpcs --filters "Name='tag:Name',Values=${VPC_NAME}" --query 'Vpcs[0].VpcId' | tr -d '"')
 
-PUBLIC_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-003e06e33a87c22f5" "Name=tag:Tier, Values=public" --query 'Subnets[*].SubnetId' | tr -d '"[]')
-PRIVATE_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-003e06e33a87c22f5" "Name=tag:Tier, Values=private" --query 'Subnets[*].SubnetId' | tr -d '"[]')
+PUBLIC_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=${VPC_ID}" "Name=tag:Tier, Values=public" --query 'Subnets[*].SubnetId' | tr -d '"[]')
+PRIVATE_SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=${VPC_ID}" "Name=tag:Tier, Values=private" --query 'Subnets[*].SubnetId' | tr -d '"[]')
 
 aws cloudformation deploy --template-file ecs-ec2-kong-app.yml --stack-name ${STACK_NAME} --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides \
